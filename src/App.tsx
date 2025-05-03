033616985e53f1850407aa72ff023141ff4a0495
bernardcryptohk <bernard.cryptohk@gmail.com>
2025-05-04 01:10:54 +0200

    Format App component code for consistency and readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import Button from './components/Button'
+import Button from "./components/Button"
 
 function App() {
   const icon = <i></i>
 
   function onClick() {
-    alert("Button clicked");
+    alert("Button clicked")
   }
 
   return (
     <>
-      <Button backgroundColor="red" fontSize={30} pillShape={true} textColor="purple" padding={[5, 10, 15, 20]}  onClick={onClick}>
-       <h1>hi</h1>
+      <Button
+        backgroundColor="red"
+        fontSize={30}
+        pillShape={true}
+        textColor="purple"
+        padding={[5, 10, 15, 20]}
+        onClick={onClick}
+      >
+        <h1>hi</h1>
       </Button>
     </>
   )
